c2ca378f3f93660f42e04e01b36aaae413e1d738
rohitmalik776 <rohitmalik776@outlook.com>
2021-12-12 22:12:55 +0530

    Learn writing to a database using sessionmaker and session

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from flask import Flask
 from sqlalchemy.engine.base import Connection
 from werkzeug.utils import escape
-from sqlalchemy import create_engine, Table, MetaData, Column, Integer, String, Boolean
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import create_engine, Table, MetaData, Column, Integer, String, Boolean, text
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 # Instance of Flask
 app = Flask(__name__)
 
 # DB connection
 engine = create_engine("sqlite+pysqlite:///database.db",
-                       echo=True, future=True)
+                       echo=False, future=True)
 
 # This is a trivial way to setup tables and metadata
 # metaData = MetaData()
@@ -32,6 +32,9 @@ class Task(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     objective = Column(String(100))
 
+    def __init__(self, objective=''):
+        self.objective = objective
+
     def __repr__(self):
         return f'id: {self.id}; objective: {self.objective}'
 
@@ -39,6 +42,32 @@ class Task(Base):
 Base.metadata.create_all(engine)
 
 # Here ends the creation (and mounting, probably) of a database
+
+# Accessing the database
+Session = sessionmaker(bind=engine)
+
+# current_session = Session()
+# clear previous enteries
+# for task in current_session.query(Task).all():
+#     print(task)
+#     current_session.delete(task)
+# current_session.execute(text('DELETE FROM task'))
+# # make a new task
+# task1 = Task()
+# task1.objective = "Hey, this is a task"
+# task2 = Task()
+# task2.objective = "Heyyyyy, another task mate"
+# task3 = Task("Third task that we have")
+
+# current_session.add(task1)
+# current_session.add(task2)
+# current_session.add(task3)
+# current_session.commit()
+
+
+@app.route('/add<get>', methods=['POST', 'GET'])
+def addTask():
+    return 'We\'ll implement Creation here'
 
 
 @app.route('/')
